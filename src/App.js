47c15b305036d4412c582032660817716657fb54
priyanka-ochaney13 <priyanka.ochaney@gmail.com>
2025-06-03 13:31:39 +0530

    made basic changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
 import './App.css';
-
+import Player from './components/Player.js';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <div id='game-container'>
+        <ol id='players'>
+          <Player name='Player 1' symbol='X' />
+          <Player name='Player 2' symbol='O' />
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
   );
 }
 
